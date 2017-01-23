05954773dafd5c27e17d1ce069700cad35e49780
ShakibHabibi <shkbhbb@gmail.com>
2017-01-23 17:33:34 +0330

    init

---
+++ src/BotKey.java
@@ -0,0 +1,17 @@
+
+public class BotKey {
+    public static final String BOT_KEY = "224158191:AAFDfN2NZIqP6oVF5D291wHqLMooRjWVIj4";
+    public static final String BOT_KEY_TEST = "270149593:AAFNpHbltk-mU2LPz-lI4E4lqKcFFkZQqVY";
+
+    public static final String BOT_NAME = "yumboxbot";
+    public static final String BOT_NAME_TEST = "yyuummbot";
+
+    public static final String DB_USER_NAME = "root";
+    public static final String DB_PASSWORD = "12345";
+
+    public static final String OWNER_ID = "75874391";
+    public static final String OWNER_ID_TEST = "69550814";
+
+    public static final long ADMIN_ID = 34364268;
+    public static final long ADMIN_ID_TEST = 69550814;
+}
